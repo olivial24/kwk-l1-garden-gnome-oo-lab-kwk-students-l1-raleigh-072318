cf4036c2fbcc279aa34e86ace277fe248b984993
olivial24 <olivia.lorenz24@gmail.com>
2018-07-26 19:43:20 +0000

    automatically backed up by learn

--- garden_gnome.rb
+++ garden_gnome.rb
@@ -1,8 +1,16 @@
 # Code your instances here
 class Gardengnome 
   
+  def name 
+    @name
+  end 
+  
   def name = (name)
     @name = name 
+  end 
+  
+  def age 
+    @age
   end 
   
   def age = (age)
@@ -13,9 +21,41 @@ class Gardengnome
     @allergy = gluten_allergy 
   end 
   
+  def gluten_allergy
+    @gluten_allergy
+  end 
+  
 def initialize.new (personality = "evil", hat_color = "red")
   @personality = personality 
   @hat_color = hat_color
 end 
 
-gnaw 
+def personality
+  @personality
+end
+
+def hat_color
+  @hat_color
+end
+
+def gnaw 
+  return "Gnawing on a tree!!!"
+end 
+
+def shout 
+  return "GNARLY!!!"
+end 
+
+def introduce_self
+  return "Hello humans, my name is #{name}, I am #{age} years old, and you'll rue the day you crossed me!"
+end 
+
+end 
+
+gnome1 = Gardengnome.new("sweet", "blue")
+gnome1.name = "Gnomeo"
+gnome.age = 16
+
+puts gnome1.name
+puts gnome1.shout
+puts gnome.gnaw 
